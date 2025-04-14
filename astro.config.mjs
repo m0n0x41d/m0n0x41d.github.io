484cda7ed9a8e610beda945252f54d9e849987eb
Ivan Zakutnii <zakutnii.ivan@gmail.com>
2025-04-14 17:59:04 +0400

    fixed errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import CompressionPlugin from "vite-plugin-compression";
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
+    output: 'static',
     i18n: {
         defaultLocale: "en",
         locales: ["en", "ru"],
@@ -10,7 +12,16 @@ export default defineConfig({
             prefixDefaultLocale: false,
         }
     },
-    integrations: [react()],
+    site: "https://ivanzakutnii.com",
+    integrations: [
+        react(),
+        sitemap({
+            filter: (page) => page !== 'https://ivanzakutnii.com/404/' &&
+                !page.includes('/404.html') &&
+                !page.includes('/404/') &&
+                !page.includes('/.html')
+        }),
+    ],
     renderers: ["@astrojs/renderer-react"],
     prerender: true,
     vite: {
